5ec0c16fe360aff92b2b55432ec47685020adf98
John Kindem <461425614@qq.com>
2020-06-21 23:15:10 +0800

    update banner component

--- frontend/component/banner.js
+++ frontend/component/banner.js
@@ -18,7 +18,7 @@ export default function Banner(props) {
             top: 0,
             left: 0,
             width: '100%',
-            height: '50%',
+            height: '70%',
             zIndex: -1,
             backgroundImage: `url(${props.background})`
         },
