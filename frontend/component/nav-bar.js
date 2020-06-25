8838ee4a551643059585a3a72881f239725ae19f
John Kindem <461425614@qq.com>
2020-06-25 16:21:06 +0800

    add affix function to nav bar

--- frontend/component/nav-bar.js
+++ frontend/component/nav-bar.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Color from '../common/style/color';
 import Font from '../common/style/font';
 import Align from '../common/style/align';
-import { Button, Row, Col, Dropdown, Menu } from 'antd';
+import { Button, Row, Col, Dropdown, Menu, Affix } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import Constant from '../common/constant';
 import Logger from '../common/utils/logger';
@@ -10,8 +10,16 @@ import '../common/style/global.css';
 import Router from '../common/utils/router';
 
 export default function() {
+    const [navBarAffixed, setNavBarAffixed] = useState(false);
+
     const style = {
+        blank: {
+            height: '1px'
+        },
         navBar: {
+            height: Align.navBarHeight,
+        },
+        navBarAffixed: {
             height: Align.navBarHeight,
             backgroundColor: Color.navBar
         },
@@ -69,30 +77,36 @@ export default function() {
         </Menu>
     );
 
+    const onAffixedChange = (affixed) => {
+        Logger.printDebug('callback', `affixed: ${affixed}`);
+        setNavBarAffixed(affixed);
+    };
+
     return (
-        <Row style={style.navBar}>
-            <Col
-                xs={{ span: 22, offset: 1 }} sm={{ span: 22, offset: 1 }} md={{ span: 22, offset: 1 }}
-                lg={{ span: 22, offset: 1 }} xl={{ span: 22, offset: 1 }} xxl={{ span: 22, offset: 1 }}>
-                <Row style={style.container}>
-                    <Col
-                         xs={{ span: 4, offset: 0 }} sm={{ span: 4, offset: 0 }} md={{ span: 4, offset: 0 }}
-                         lg={{ span: 4, offset: 0 }} xl={{ span: 4, offset: 0 }} xxl={{ span: 4, offset: 0 }}>
-                        <Row id={'logo'} style={style.logo}>
-                            <a href={Constant.route.index} style={style.logoLink}>{Constant.text.logo}</a>
-                        </Row>
-                    </Col>
-                    <Col
-                         xs={{ span: 0, offset: 0 }} sm={{ span: 0, offset: 0 }} md={{ span: 20, offset: 0 }}
-                         lg={{ span: 20, offset: 0 }} xl={{ span: 20, offset: 0 }} xxl={{ span: 20, offset: 0 }}>
-                        <Row style={style.nav}>
-                            {Constant.other.navLink.map((link, key) => {
-                                Logger.printDebug('map', `create nav link ${key}-${link.name}`);
-                                return (
-                                    <a key={key} href={link.to} style={style.navLink}>{link.name}</a>
-                                );
-                            })}
-                            <span style={style.navBtnSpan}>
+        <Affix offsetTop={0} onChange={onAffixedChange}>
+            <Row style={navBarAffixed ? style.navBarAffixed : style.navBar}>
+                <Col
+                    xs={{ span: 22, offset: 1 }} sm={{ span: 22, offset: 1 }} md={{ span: 22, offset: 1 }}
+                    lg={{ span: 22, offset: 1 }} xl={{ span: 22, offset: 1 }} xxl={{ span: 22, offset: 1 }}>
+                    <Row style={style.container}>
+                        <Col
+                            xs={{ span: 4, offset: 0 }} sm={{ span: 4, offset: 0 }} md={{ span: 4, offset: 0 }}
+                            lg={{ span: 4, offset: 0 }} xl={{ span: 4, offset: 0 }} xxl={{ span: 4, offset: 0 }}>
+                            <Row id={'logo'} style={style.logo}>
+                                <a href={Constant.route.index} style={style.logoLink}>{Constant.text.logo}</a>
+                            </Row>
+                        </Col>
+                        <Col
+                            xs={{ span: 0, offset: 0 }} sm={{ span: 0, offset: 0 }} md={{ span: 20, offset: 0 }}
+                            lg={{ span: 20, offset: 0 }} xl={{ span: 20, offset: 0 }} xxl={{ span: 20, offset: 0 }}>
+                            <Row style={style.nav}>
+                                {Constant.other.navLink.map((link, key) => {
+                                    Logger.printDebug('map', `create nav link ${key}-${link.name}`);
+                                    return (
+                                        <a key={key} href={link.to} style={style.navLink}>{link.name}</a>
+                                    );
+                                })}
+                                <span style={style.navBtnSpan}>
                                 {Constant.other.navBtn.map((btn, key) => {
                                     Logger.printDebug('map', `create nav btn ${key}-${btn.name}`);
                                     return (
@@ -109,19 +123,20 @@ export default function() {
                                     );
                                 })}
                             </span>
-                        </Row>
-                    </Col>
-                    <Col
-                        xs={{ span: 20, offset: 0 }} sm={{ span: 20, offset: 0 }} md={{ span: 0, offset: 0 }}
-                        lg={{ span: 0, offset: 0 }} xl={{ span: 0, offset: 0 }} xxl={{ span: 0, offset: 0 }}>
-                        <Row style={style.nav}>
-                            <Dropdown overlay={navBarDropdownMenu}>
-                                <a style={style.navLink}>{Constant.text.menu} <DownOutlined/></a>
-                            </Dropdown>
-                        </Row>
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
+                            </Row>
+                        </Col>
+                        <Col
+                            xs={{ span: 20, offset: 0 }} sm={{ span: 20, offset: 0 }} md={{ span: 0, offset: 0 }}
+                            lg={{ span: 0, offset: 0 }} xl={{ span: 0, offset: 0 }} xxl={{ span: 0, offset: 0 }}>
+                            <Row style={style.nav}>
+                                <Dropdown overlay={navBarDropdownMenu}>
+                                    <a style={style.navLink}>{Constant.text.menu} <DownOutlined/></a>
+                                </Dropdown>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Affix>
     );
 }
