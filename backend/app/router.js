c94863aa6f4a29375f9583ccfd650b90bff1b999
John Kindem <461425614@qq.com>
2020-07-19 01:39:34 +0800

    remove bad backend project

--- backend/app/router.js
+++
@@ -1,4 +0,0 @@
-module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
-};
--- backend/config/plugin.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    sequelize: {
-        enable: true,
-        package: 'egg-sequelize'
-    }
-};
