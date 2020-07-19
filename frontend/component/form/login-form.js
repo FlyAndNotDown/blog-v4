bc38c30662f98be49d7213803e1a8cf393c54e20
John Kindem <461425614@qq.com>
2020-07-19 13:32:02 +0800

    add login pages and login form component

---
+++ frontend/component/form/login-form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../common/style/component/form/login-form.css';
+import { Row, Col } from 'antd';
+
+
+export function LoginForm(props) {
+    return (
+        <Row>
+            <Col span={24}>
+
+            </Col>
+        </Row>
+    );
+}
--- frontend/pages/user/login.js
+++ frontend/pages/user/login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../../common/style/global.css';
+import '../../common/style/pages/user/login.css';
+import { Row, Col } from 'antd';
+import { LoginForm } from '../../component/form/login-form';
 
 function UserLogin() {
     return (
-        <div>
-        </div>
+        <Row className={'login-container'}>
+            <Col className={'login-col'}
+                 xs={{ span: 22, offset: 1 }} sm={{ span: 22, offset: 1 }} md={{ span: 20, offset: 2 }}
+                 lg={{ span: 12, offset: 6 }} xl={{ span: 8, offset: 8 }} xxl={{ span: 6, offset: 9 }}>
+                <div className={'login-form-container'}>
+                    <LoginForm/>
+                </div>
+            </Col>
+        </Row>
     );
 }
 
