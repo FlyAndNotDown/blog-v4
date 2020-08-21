0cc16a53d0391f5844c6975b2f7110de4dd3b1d6
John Kindem <461425614@qq.com>
2020-08-22 02:02:31 +0800

    add body component

--- frontend/pages/archive.js
+++ frontend/pages/archive.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { NavBar } from '../component/nav/nav-bar';
+import {Body} from '../component/container/body';
 
 function Archive() {
     return (
-        <NavBar alwaysAffixed={true}/>
+        <div>
+            <NavBar alwaysAffixed={true}/>
+            <Body>
+
+            </Body>
+        </div>
     );
 }
 
