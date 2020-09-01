8579360415719e322cd0524bc2c852d3eb47254e
John Kindem <461425614@qq.com>
2020-09-01 23:46:41 +0800

    update tag and tag-list component

--- frontend/component/display/tag-list.js
+++ frontend/component/display/tag-list.js
@@ -4,11 +4,26 @@ import { Tag } from './tag';
 
 export function TagList(props) {
     const tags = props.tags || [];
+    const onClick = props.onClick || (() => {});
+
+    const tagGenerator = (tag, key) => {
+        const onClickInternal = () => {
+            onClick(key, tag);
+        };
+
+        return (
+            <Tag
+                key={key}
+                onClick={onClickInternal}>
+                {tag.name}
+            </Tag>
+        );
+    };
 
     return (
         <div className={Style.main}>
             {tags.map((tag, key) => {
-                return <Tag key={key}>{tag.name}</Tag>;
+                return tagGenerator(tag, key);
             })}
         </div>
     );
--- frontend/component/display/tag.js
+++ frontend/component/display/tag.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Style from './tag.module.css';
 
 export function Tag(props) {
+    const onClick = props.onClick || (() => {});
+
     return (
-        <div className={Style.main}>
+        <div
+            className={Style.main}
+            onClick={onClick}>
             {props.children}
         </div>
     );
