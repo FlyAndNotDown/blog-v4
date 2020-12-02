d04600751a8c5c6d931309749a0872c75b801878
John Kindem <461425614@qq.com>
2020-12-02 23:08:47 +0800

    add framework for message page

---
+++ frontend/common/mock/data/message.js
@@ -0,0 +1,14 @@
+export default {
+    common: {
+        friends: [{
+            name: '栗子木凤凰芯',
+            to: 'https://chestnut-phoenix.xyz/'
+        }, {
+            name: '网站架构',
+            to: 'http://www.architecy.com/'
+        }, {
+            name: '虎书博客',
+            to: 'http://www.tigerbook.cn/'
+        }]
+    }
+};
--- frontend/pages/message.js
+++ frontend/pages/message.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { NavBar } from '../component/nav/nav-bar';
 import { Body } from '../component/container/body';
+import { BlogConfig } from '../blog.config';
+import { MockData } from '../common/mock/mock';
+import { Content } from '../component/container/content';
+import { Footer } from '../component/display/footer';
 
-function MessagePage() {
+function MessagePage(props) {
+    const friends = props.common.friends || [];
+
     return (
         <div>
             <NavBar alwaysAffixed={true}/>
             <Body>
+                <Content>
 
+                </Content>
+                <Footer friends={friends}/>
             </Body>
         </div>
     );
 }
+
+MessagePage.getInitialProps = async () => {
+    return BlogConfig.useMockData ? MockData.message : {};
+};
 
 export default MessagePage;
