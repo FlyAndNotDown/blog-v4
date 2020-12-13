00507f4aa08c3825dc0b9c9636a1357d6c616a72
John Kindem <461425614@qq.com>
2020-12-13 23:31:26 +0800

    update code about user controller

--- backend/app/controller/user.js
+++ backend/app/controller/user.js
@@ -3,22 +3,81 @@
 const Controller = require('egg').Controller;
 
 class UserController extends Controller {
-  async get() {
-    const { ctx } = this;
-    // TODO
-    ctx.body = {};
-  }
-
   async getLogin() {
     const { ctx } = this;
-    // TODO
-    ctx.body = {};
+
+    const userLogin = !!ctx.session.userLogin;
+    const userPk = ctx.session.userPk || -1;
+
+    if (!(userPk > 0)) {
+      ctx.body = {
+        success: false,
+        reason: 'bad user info in session',
+      };
+      return;
+    }
+
+    if (userLogin) {
+      const user = ctx.model.User.findByPk(userPk);
+      if (!user) {
+        ctx.body = {
+          success: false,
+          reason: 'bad user info in session',
+        };
+        return;
+      }
+
+      ctx.body = {
+        success: true,
+        content: {
+          login: true,
+          info: ctx.model.User.findByPk(userPk, {
+            attributes: [
+              'username',
+              'avatar',
+            ],
+          }),
+        },
+      };
+    } else {
+      ctx.body = {
+        success: true,
+        content: {
+          login: false,
+          info: null,
+        },
+      };
+    }
   }
 
   async postLoginEmail() {
     const { ctx } = this;
-    // TODO
-    ctx.body = {};
+
+    const email = ctx.request.body.email || '';
+    const password = ctx.request.body.password || '';
+
+    const user = ctx.model.User.getEmailUser(email);
+    if (!user) {
+      ctx.body = {
+        result: false,
+        reason: 'email is not exists',
+      };
+      return;
+    }
+
+    if (password !== user.password) {
+      ctx.body = {
+        result: false,
+        reason: 'bad password',
+      };
+      return;
+    }
+
+    ctx.session.userLogin = true;
+    ctx.session.userPk = user.pk;
+    ctx.body = {
+      result: true,
+    };
   }
 
   async postLoginGithub() {
@@ -35,8 +94,12 @@ class UserController extends Controller {
 
   async deleteLogin() {
     const { ctx } = this;
-    // TODO
-    ctx.body = {};
+
+    ctx.session.userLogin = false;
+    ctx.session.userPk = null;
+    ctx.body = {
+      result: true,
+    };
   }
 }
 
