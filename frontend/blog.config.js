ce08925368b89dfd71f234deff5a12a766abb71b
John Kindem <461425614@qq.com>
2021-02-28 02:44:03 +0800

    fix a bug

--- frontend/blog.config.js
+++ frontend/blog.config.js
@@ -1,5 +1,5 @@
 export const BlogConfig = {
-  debugMode: process.env.DEBUG_MODE,
+  debugMode: process.env.DEBUG_MODE === 'true',
   host: process.env.HOST,
   backendHost: process.env.BACKEND_HOST,
 };
